cc33a99b9bb3e78ff8af16fd31df4ccb26a7dce4
devilofrdx12 <devilofrdx12@gmail.com>
2026-03-19 13:53:02 +0530

    Updated the wave animation and speed  Your branch is up to date with 'devilofrdx12/main'.
     Changes to be committed:
    	modified:   src/components/AnimatedWave.jsx
    	modified:   src/components/DesktopGrid.css

--- src/components/AnimatedWave.jsx
+++ src/components/AnimatedWave.jsx
@@ -17,11 +17,11 @@ const AnimatedWave = ({ theme }) => {
         paused={false}
         options={{
           height: 20,       
-          amplitude: 30,    
-          speed: 0.10,      
-          points: 4         
+          amplitude: 22,    
+          speed: 0.27,      
+          points: 2         
         }}
-        style={{ width: '100%', height: '100%', display: 'flex' }}
+        style={{ width: '100%', height: '100%', display: 'flex', transform: 'translateZ(0)' }}
       >
        <defs>
           <linearGradient id="smart-gradient" gradientTransform="rotate(90)">
@@ -30,21 +30,21 @@ const AnimatedWave = ({ theme }) => {
             <stop 
               offset="8%" 
               stopColor={theme === 'light' ? "#A0D2FF" : "#ADF1FF"} 
-              style={{ transition: 'stop-color 0.5s ease-in-out' }} 
+              style={{ transition: 'stop-color 0.5s ease-in-out', transform: 'translateZ(0)' }} 
             />
             
             {/* Middle section - Hard stop at 8% never moves! */}
             <stop 
               offset="8%" 
               stopColor={theme === 'light' ? "#A0D2FF" : "#356586"} 
-              style={{ transition: 'stop-color 0.5s ease-in-out' }} 
+              style={{ transition: 'stop-color 0.5s ease-in-out', transform: 'translateZ(0)' }} 
             />
 
             {/* Bottom deep water */}
             <stop 
               offset="90%" 
               stopColor={theme === 'light' ? "#C7E4FF" : "#0C3256"} 
-              style={{ transition: 'stop-color 0.5s ease-in-out' }} 
+              style={{ transition: 'stop-color 0.5s ease-in-out', transform: 'translateZ(0)' }} 
             />
             
           </linearGradient>
